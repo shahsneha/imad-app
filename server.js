8106ef2c7f9f0ff453441ec33a0537c06089d678
shahsneha <30995645+shahsneha@users.noreply.github.com>
2017-08-15 05:30:31 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article_one',function(req,res) { res.sendFile('This is request for article one and it will be served');});
+app.get('/article_one',function(req,res) { res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));});
 app.get('/article_two',function(req,res) { res.sendFile('This is request for article two and it will be served');});
 app.get('/article_three',function(req,res) { res.sendFile('This is request for article three and it will be served');});
 app.get('/ui/madi.png', function (req, res) {
